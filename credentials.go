6a3abb43a4912f21c4143cc6b65be2f463e8a865
mo <thinkgo@aliyun.com>
2020-04-21 22:31:04 +0800

    fix userIP --> userAddr like host:port

--- credentials.go
+++ credentials.go
@@ -1,7 +1,7 @@
 package socks5
 
-// CredentialStore is used to support user/pass authentication optional user ip
-// if you want to limit user ip ,you can refuse it.
+// CredentialStore is used to support user/pass authentication optional network addr
+// if you want to limit user network addr,you can refuse it.
 type CredentialStore interface {
 	Valid(user, password, userIP string) bool
 }
@@ -9,7 +9,7 @@ type CredentialStore interface {
 // StaticCredentials enables using a map directly as a credential store
 type StaticCredentials map[string]string
 
-func (s StaticCredentials) Valid(user, password, userIP string) bool {
+func (s StaticCredentials) Valid(user, password, userAddr string) bool {
 	pass, ok := s[user]
 	if !ok {
 		return false
