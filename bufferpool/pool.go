91297c7e4f29c9d8e529d2dfdfab7fc8fee63f15
Adm0 <12154400+Adm0@users.noreply.github.com>
2025-10-27 20:56:40 +0500

    fix: Memory leaks in BufPool
    Storing non-pointer values in `sync.Pool` allocates memory. (SCC-SA6002)

--- bufferpool/pool.go
+++ bufferpool/pool.go
@@ -22,13 +22,16 @@ func NewPool(size int) BufPool {
 	return &pool{
 		size,
 		&sync.Pool{
-			New: func() interface{} { return make([]byte, 0, size) }},
+			New: func() any {
+				b := make([]byte, 0, size)
+				return &b
+			}},
 	}
 }
 
 // Get implement interface BufPool
 func (sf *pool) Get() []byte {
-	return sf.pool.Get().([]byte)
+	return *(sf.pool.Get().(*[]byte))
 }
 
 // Put implement interface BufPool
@@ -36,5 +39,6 @@ func (sf *pool) Put(b []byte) {
 	if cap(b) != sf.size {
 		panic("invalid buffer size that's put into leaky buffer")
 	}
-	sf.pool.Put(b[:0]) //nolint: staticcheck
+	b = b[:0]
+	sf.pool.Put(&b) //nolint: staticcheck
 }
